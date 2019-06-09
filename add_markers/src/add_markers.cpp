09db2a96fcdd2f7f6182fdc77847313ae127b4c3
Pankaj Chopra <choprapkj@gmail.com>
2019-06-09 15:38:41 +0000

    add_marker subscribes to message /odom

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -27,100 +27,97 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// %Tag(FULLTEXT)%
-// %Tag(INCLUDES)%
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
-// %EndTag(INCLUDES)%
+#include <nav_msgs/Odometry.h>
 
-// %Tag(INIT)%
+float odom_x = 0.0;
+float odom_y = 0.0;
+
+visualization_msgs::Marker setMarkerProperties(const uint32_t shape){
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "object_marker";
+  marker.id = 0;
+
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker.pose.position.x = 1.0;
+  marker.pose.position.y = 0;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 0.25;
+  marker.scale.y = 0.25;
+  marker.scale.z = 0.25;
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker.color.r = 0.0f;
+  marker.color.g = 1.0f;
+  marker.color.b = 0.0f;
+  marker.color.a = 1.0;
+  marker.lifetime = ros::Duration();
+  return marker;
+}
+
+void odom_sub_callback(const nav_msgs::Odometry::ConstPtr& message)
+{
+  odom_x = message->pose.pose.position.y;
+  odom_y = message->pose.pose.position.x;
+}
+  
 int main( int argc, char** argv )
 {
-  ros::init(argc, argv, "basic_shapes");
+  ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-// %EndTag(INIT)%
-
+  ros::Subscriber odom_sub = n.subscribe("/odom",1000,odom_sub_callback);
   // Set our initial shape type to be a cube
-// %Tag(SHAPE_INIT)%
   uint32_t shape = visualization_msgs::Marker::CUBE;
-// %EndTag(SHAPE_INIT)%
 
-// %Tag(MARKER_INIT)%
-  while (ros::ok())
+  visualization_msgs::Marker marker;
+  marker = setMarkerProperties(shape);
+  // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+  marker.action = visualization_msgs::Marker::ADD;
+  
+  // Publish the marker
+  //while (marker_pub.getNumSubscribers() < 1)
   {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-// %EndTag(MARKER_INIT)%
-
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-// %Tag(NS_ID)%
-    marker.ns = "basic_shapes";
-    marker.id = 0;
-// %EndTag(NS_ID)%
-
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-// %Tag(TYPE)%
-    marker.type = shape;
-// %EndTag(TYPE)%
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-// %Tag(ACTION)%
-    marker.action = visualization_msgs::Marker::ADD;
-// %EndTag(ACTION)%
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-// %Tag(POSE)%
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-// %EndTag(POSE)%
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-// %Tag(SCALE)%
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-// %EndTag(SCALE)%
-
-    // Set the color -- be sure to set alpha to something non-zero!
-// %Tag(COLOR)%
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
-// %EndTag(COLOR)%
-
-// %Tag(LIFETIME)%
-    marker.lifetime = ros::Duration();
-// %EndTag(LIFETIME)%
-
-    // Publish the marker
-// %Tag(PUBLISH)%
-    while (marker_pub.getNumSubscribers() < 1)
+    if (!ros::ok())
     {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
+      return 0;
     }
-    marker_pub.publish(marker);
-// %EndTag(PUBLISH)%
-
-
-// %Tag(SLEEP_END)%
-    r.sleep();
+    ROS_WARN_ONCE("Please create a subscriber to the marker1");
+    sleep(1);
   }
-// %EndTag(SLEEP_END)%
+  marker_pub.publish(marker);
+
+  // PC work
+  sleep(10);
+  marker.action = visualization_msgs::Marker::DELETE;
+  marker.pose.position.x = -1;
+  marker.action = visualization_msgs::Marker::ADD;
+
+  //while (marker_pub.getNumSubscribers() < 1)
+  {
+    if (!ros::ok())
+    {
+      return 0;
+    }
+    ROS_WARN_ONCE("Please create a subscriber to the marker2");
+    sleep(1);
+  }
+  marker_pub.publish(marker);
+
+  ros::spin();
 }
-// %EndTag(FULLTEXT)%
+
